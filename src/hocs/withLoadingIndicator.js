0aad9601b41fb9e8c67630a062fb7cfea53dc79c
SuranartN <suranartnc@gmail.com>
2017-10-30 20:00:11 +0700

    Replace tabs with spaces

--- src/hocs/withLoadingIndicator.js
+++ src/hocs/withLoadingIndicator.js
@@ -4,24 +4,24 @@ import { branch, renderComponent, compose } from 'recompose'
 import { StyleSheet, View, ActivityIndicator } from 'react-native'
 
 function Preloader() {
-	return (
-		<View style={styles.container}>
-			<ActivityIndicator />
-		</View>
-	)
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator />
+    </View>
+  )
 }
 
 export default function() {
-	return compose(
-		branch(props => {
-			return props.data.loading
-		}, renderComponent(Preloader))
-	)
+  return compose(
+    branch(props => {
+      return props.data.loading
+    }, renderComponent(Preloader))
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		paddingTop: 20
-	}
+  container: {
+    flex: 1,
+    paddingTop: 20
+  }
 })
