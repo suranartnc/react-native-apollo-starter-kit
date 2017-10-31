b86ec25ba1354d119e76b9b8915ed08aedbdba22
SuranartN <suranartnc@gmail.com>
2017-10-31 11:32:07 +0700

    Add some styles for container

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,36 @@
-import React from 'react'
+// imr
+import React, { Component } from 'react'
+
+// imrn
+import { StyleSheet, View, Alert } from 'react-native'
 
 import { SocialIcon } from 'react-native-elements'
 
-function LoginScreen() {
-  return <SocialIcon button title="Sign In With GitHub" type="github" />
+// rcc
+class LoginScreen extends Component {
+  handleSubmitButton = () => {
+    Alert.alert('Sign In With GitHub')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <SocialIcon
+          button
+          title="Sign In With GitHub"
+          type="github"
+          onPress={this.handleSubmitButton}
+        />
+      </View>
+    )
+  }
 }
+
+// rnss
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 export default LoginScreen
